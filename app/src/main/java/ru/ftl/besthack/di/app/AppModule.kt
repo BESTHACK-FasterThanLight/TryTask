aa80b6ade5e6302c93349284feab1e3f4826c5c3
lionzxy <nikita@kulikof.ru>
2018-04-04 20:36:43 +0300

    Fix build

--- app/src/main/java/ru/ftl/besthack/di/app/AppModule.kt
+++ app/src/main/java/ru/ftl/besthack/di/app/AppModule.kt
@@ -6,11 +6,6 @@ import android.content.SharedPreferences
 import android.preference.PreferenceManager
 import dagger.Module
 import dagger.Provides
-import okhttp3.CertificatePinner
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import ru.ftl.besthack.data.db.AppDatabase
 import javax.inject.Singleton
 
